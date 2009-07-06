dfcef442881cd3cddf341806b69f9e933babe681
bob <KrabbyKrap@gmail.com>
2009-07-05 21:40:33 -0600

    Don't apply camera transformations to the GUI

--- tankbobs/src/client/st_play.lua
+++ tankbobs/src/client/st_play.lua
@@ -329,29 +329,14 @@ function st_play_step(d)
 
 	c_world_step(d)
 
-	-- adjust the camera
-	local uppermost
-	local lowermost
-	local rightmost
-	local leftmost
-	-- Position the camera so that all tanks and powerups are shown, while zooming in as much as possible
-	for _, v in pairs(c_world_getTanks()) do
-		if not leftmost or v.p[1].x < leftmost then
-			leftmost = v.p[1].x
-		end
-		if not rightmost or v.p[1].x > rightmost then
-			rightmost = v.p[1].x
-		end
-
-		if not lowermost or v.p[1].y < lowermost then
-			lowermost = v.p[1].y
-		end
-		if not uppermost or v.p[1].y > uppermost then
-			uppermost = v.p[1].y
-		end
-	end
-	for _, v in pairs(c_world_getPowerups()) do
-		if v.spawner.focus then
+	gl.PushMatrix()
+		-- adjust the camera
+		local uppermost
+		local lowermost
+		local rightmost
+		local leftmost
+		-- Position the camera so that all tanks and powerups are shown, while zooming in as much as possible
+		for _, v in pairs(c_world_getTanks()) do
 			if not leftmost or v.p[1].x < leftmost then
 				leftmost = v.p[1].x
 			end
@@ -366,216 +351,233 @@ function st_play_step(d)
 				uppermost = v.p[1].y
 			end
 		end
-	end
-	if not uppermost or not lowermost or not rightmost or not leftmost then
-		return
-	end
-	local m = c_tcm_current_map
-	-- FIXME: this is broken
-	--uppermost = math.min(m.uppermost - 95, uppermost)
-	--lowermost = math.max(m.lowermost + 95, lowermost)
-	--rightmost = math.min(m.rightmost - 95, rightmost)
-	--leftmost  = math.max(m.leftmost  + 95,  leftmost)
+		for _, v in pairs(c_world_getPowerups()) do
+			if v.spawner.focus then
+				if not leftmost or v.p[1].x < leftmost then
+					leftmost = v.p[1].x
+				end
+				if not rightmost or v.p[1].x > rightmost then
+					rightmost = v.p[1].x
+				end
 
-	gl.Translate(50, 50, 0)
-
-	local distance = math.abs(rightmost - leftmost) > math.abs(uppermost - lowermost) and math.abs(rightmost - leftmost) or math.abs(uppermost - lowermost)
-	local scale = 100 / (distance + c_config_get("config.client.cameraExtraFOV"))
-	if scale > 1 then
-		scale = 1
-	end
-	zoom = common_lerp(zoom, scale, d * c_config_get("config.client.cameraSpeed"))
-	gl.Scale(zoom, zoom, 1)
-
-	camera = common_lerp(camera, tankbobs.m_vec2(-(rightmost + leftmost) / 2, -(uppermost + lowermost) / 2), d * c_config_get("config.client.cameraSpeed"))
-	gl.Translate(camera.x, camera.y, 0)
-
-	-- draw tanks and walls
-	for i = 1, c_const_get("tcm_maxLevel") do
-		for k, v in pairs(c_tcm_current_map.walls) do
-			if i == c_const_get("tcm_tankLevel") then
-				-- render tanks
-				for k, v in pairs(c_world_getTanks()) do
-					if(v.exists) then
-						gl.PushAttrib("CURRENT_BIT")
-							gl.PushMatrix()
-								gl.Translate(v.p[1].x, v.p[1].y, 0)
-								gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
-								if not (c_config_get("config.game.player" .. tostring(k) .. ".color", nil, true)) then
-									c_config_set("config.game.player" .. tostring(k) .. ".color.r", c_config_get("config.game.defaultTankRed"))
-									c_config_set("config.game.player" .. tostring(k) .. ".color.g", c_config_get("config.game.defaultTankBlue"))
-									c_config_set("config.game.player" .. tostring(k) .. ".color.b", c_config_get("config.game.defaultTankGreen"))
-									c_config_set("config.game.player" .. tostring(k) .. ".color.a", c_config_get("config.game.defaultTankAlpha"))
-								end
-								gl.Color(v.color.r, v.color.g, v.color.b, 1)
-								gl.TexEnv("TEXTURE_ENV_COLOR", v.color.r, v.color.g, v.color.b, 1)
-								-- blend color with tank texture
-								gl.CallList(tank_listBase)
-								-- white outline
-								gl.Color(1, 1, 1, 0.875)
-								gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1, 0.875)
-								gl.CallList(tankBorder_listBase)
-
-								if v.weapon then
-									gl.CallList(v.weapon.m.p.list)
-								end
-							gl.PopMatrix()
-						gl.PopAttrib()
-					end
+				if not lowermost or v.p[1].y < lowermost then
+					lowermost = v.p[1].y
+				end
+				if not uppermost or v.p[1].y > uppermost then
+					uppermost = v.p[1].y
 				end
 			end
-
-			if v.l == i and v.m.pos then
-				gl.Color(1, 1, 1, 1)
-				gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1)
-				gl.BindTexture("TEXTURE_2D", v.m.texture)
-
-				gl.EnableClientState("VERTEX_ARRAY,TEXTURE_COORD_ARRAY")
-				w[1][1], w[1][2] = v.m.pos[1].x, v.m.pos[1].y
-				w[2][1], w[2][2] = v.m.pos[2].x, v.m.pos[2].y
-				w[3][1], w[3][2] = v.m.pos[3].x, v.m.pos[3].y
-				t[1][1], t[1][2] = v.t[1].x, v.t[1].y
-				t[2][1], t[2][2] = v.t[2].x, v.t[2].y
-				t[3][1], t[3][2] = v.t[3].x, v.t[3].y
-				if v.m.pos[4] then
-					w[4][1], w[4][2] = v.m.pos[4].x, v.m.pos[4].y
-				else
-					t[4][1], t[4][2] = v.t[4].x, v.t[4].y
-				end
-				gl.VertexPointer(w)
-				gl.TexCoordPointer(t)
-				gl.DrawArrays("POLYGON", 0, #v.m.pos)  -- TODO: FIXME: figure out why texture coordinates are being ignored and remove immediate mode below
-				gl.DisableClientState("VERTEX_ARRAY,TEXTURE_COORD_ARRAY")
-
-				gl.Begin(v.m.pos[4] and "QUADS" or "TRIANGLES")
-					gl.TexCoord(v.t[1].x, v.t[1].y) gl.Vertex(v.m.pos[1].x, v.m.pos[1].y)
-					gl.TexCoord(v.t[2].x, v.t[2].y) gl.Vertex(v.m.pos[2].x, v.m.pos[2].y)
-					gl.TexCoord(v.t[3].x, v.t[3].y) gl.Vertex(v.m.pos[3].x, v.m.pos[3].y)
-					if v.m.pos[4] then
-						gl.TexCoord(v.t[4].x, v.t[4].y) gl.Vertex(v.m.pos[4].x, v.m.pos[4].y)
-					end
-				gl.End()
-			end
 		end
-	end
-
-	-- aiming aids
-	gl.EnableClientState("VERTEX_ARRAY")
-	for _, v in pairs(c_world_getTanks()) do
-		if v.exists then
-			if (v.weapon and v.weapon.aimAid) or (v.cd.aimAid) then
-				gl.PushAttrib("ENABLE_BIT")
-					local b
-					local vec = tankbobs.m_vec2()
-					local start, endP = tankbobs.m_vec2(v.p[1]), tankbobs.m_vec2()
-	
-					vec.t = v.r
-					vec.R = c_const_get("aimAid_startDistance")
-					start:add(vec)
-	
-					endP(start)
-					vec.R = c_const_get("aimAid_maxDistance")
-					endP:add(vec)
-	
-					b, vec = c_world_findClosestIntersection(start, endP)
-					if b then
-						endP = vec
-					end
-	
-					a[1][1] = start.x a[1][2] = start.y
-					a[2][1] = endP.x a[2][2] = endP.y
-					gl.Disable("TEXTURE_2D")
-					gl.Color(0.9, 0.1, 0.1, 1)
-					gl.TexEnv("TEXTURE_ENV_COLOR", 0.9, 0.1, 0.1, 1)
-					gl.VertexPointer(a)
-					gl.LineWidth(c_const_get("aimAid_width"))
-					gl.DrawArrays("LINES", 0, 2)
-				gl.PopAttrib()
-			end
-		end
-	end
-	gl.DisableClientState("VERTEX_ARRAY")
-
-	-- teleporters are drawn on top everything above
-	--gl.CallLists(play_teleporter_listsMultiple)
-
-	-- powerups are drawn next
-	for _, v in pairs(c_world_getPowerups()) do
-		gl.PushMatrix()
-			local c = c_world_getPowerupTypeByName(v.typeName).c
-			gl.Color(c.r, c.g, c.b, c.a)
-			gl.TexEnv("TEXTURE_ENV_COLOR", c.r, c.g, c.b, c.a)
-			gl.Translate(v.p[1].x, v.p[1].y, 0)
-			gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
-			gl.CallList(powerup_listBase)
-		gl.PopMatrix()
-	end
-
-	-- projectiles
-	for _, v in pairs(c_weapon_getProjectiles()) do
-		if v.weapon.trail == 0 then  -- only draw the trail
-			gl.PushMatrix()
-				gl.Translate(v.p[1].x, v.p[1].y, 0)
-				gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
-				gl.CallList(v.weapon.m.p.projectileList)
-			gl.PopMatrix()
-		end
-	end
-
-	-- trails
-	for k, v in pairs(trails) do
-		-- {time left, maximum intensity, list}
-		v[1] = v[1] - d
-		if v[1] <= 0 then
-			gl.DeleteLists(v[3], 1)
-
-			table.remove(trails, k)
-
+		if not uppermost or not lowermost or not rightmost or not leftmost then
 			return
 		end
+		local m = c_tcm_current_map
+		-- FIXME: this is broken
+		--uppermost = math.min(m.uppermost - 95, uppermost)
+		--lowermost = math.max(m.lowermost + 95, lowermost)
+		--rightmost = math.min(m.rightmost - 95, rightmost)
+		--leftmost  = math.max(m.leftmost  + 95,  leftmost)
 
-		gl.Color(1, 1, 1, v[1] / v[2])
-		gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1, v[2] / v[3])
-		gl.CallList(v[3])
-	end
+		gl.Translate(50, 50, 0)
 
-	-- healthbars
-	for k, v in pairs(c_world_getTanks()) do
-		if v.exists then
+	local distance = math.abs(rightmost - leftmost) > math.abs(uppermost - lowermost) and math.abs(rightmost - leftmost) or math.abs(uppermost - lowermost)
+		local scale = 100 / (distance + c_config_get("config.client.cameraExtraFOV"))
+		if scale > 1 then
+			scale = 1
+		end
+		zoom = common_lerp(zoom, scale, d * c_config_get("config.client.cameraSpeed"))
+		gl.Scale(zoom, zoom, 1)
+
+		camera = common_lerp(camera, tankbobs.m_vec2(-(rightmost + leftmost) / 2, -(uppermost + lowermost) / 2), d * c_config_get("config.client.cameraSpeed"))
+		gl.Translate(camera.x, camera.y, 0)
+
+		-- draw tanks and walls
+		for i = 1, c_const_get("tcm_maxLevel") do
+			for k, v in pairs(c_tcm_current_map.walls) do
+				if i == c_const_get("tcm_tankLevel") then
+					-- render tanks
+					for k, v in pairs(c_world_getTanks()) do
+						if(v.exists) then
+							gl.PushAttrib("CURRENT_BIT")
+								gl.PushMatrix()
+									gl.Translate(v.p[1].x, v.p[1].y, 0)
+									gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
+									if not (c_config_get("config.game.player" .. tostring(k) .. ".color", nil, true)) then
+										c_config_set("config.game.player" .. tostring(k) .. ".color.r", c_config_get("config.game.defaultTankRed"))
+										c_config_set("config.game.player" .. tostring(k) .. ".color.g", c_config_get("config.game.defaultTankBlue"))
+										c_config_set("config.game.player" .. tostring(k) .. ".color.b", c_config_get("config.game.defaultTankGreen"))
+										c_config_set("config.game.player" .. tostring(k) .. ".color.a", c_config_get("config.game.defaultTankAlpha"))
+									end
+									gl.Color(v.color.r, v.color.g, v.color.b, 1)
+									gl.TexEnv("TEXTURE_ENV_COLOR", v.color.r, v.color.g, v.color.b, 1)
+									-- blend color with tank texture
+									gl.CallList(tank_listBase)
+									-- white outline
+									gl.Color(1, 1, 1, 0.875)
+									gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1, 0.875)
+									gl.CallList(tankBorder_listBase)
+
+									if v.weapon then
+										gl.CallList(v.weapon.m.p.list)
+									end
+								gl.PopMatrix()
+							gl.PopAttrib()
+						end
+					end
+				end
+
+				if v.l == i and v.m.pos then
+					gl.Color(1, 1, 1, 1)
+					gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1)
+					gl.BindTexture("TEXTURE_2D", v.m.texture)
+
+					gl.EnableClientState("VERTEX_ARRAY,TEXTURE_COORD_ARRAY")
+					w[1][1], w[1][2] = v.m.pos[1].x, v.m.pos[1].y
+					w[2][1], w[2][2] = v.m.pos[2].x, v.m.pos[2].y
+					w[3][1], w[3][2] = v.m.pos[3].x, v.m.pos[3].y
+					t[1][1], t[1][2] = v.t[1].x, v.t[1].y
+					t[2][1], t[2][2] = v.t[2].x, v.t[2].y
+					t[3][1], t[3][2] = v.t[3].x, v.t[3].y
+					if v.m.pos[4] then
+						w[4][1], w[4][2] = v.m.pos[4].x, v.m.pos[4].y
+					else
+						t[4][1], t[4][2] = v.t[4].x, v.t[4].y
+					end
+					gl.VertexPointer(w)
+					gl.TexCoordPointer(t)
+					gl.DrawArrays("POLYGON", 0, #v.m.pos)  -- TODO: FIXME: figure out why texture coordinates are being ignored and remove immediate mode below
+					gl.DisableClientState("VERTEX_ARRAY,TEXTURE_COORD_ARRAY")
+
+					gl.Begin(v.m.pos[4] and "QUADS" or "TRIANGLES")
+						gl.TexCoord(v.t[1].x, v.t[1].y) gl.Vertex(v.m.pos[1].x, v.m.pos[1].y)
+						gl.TexCoord(v.t[2].x, v.t[2].y) gl.Vertex(v.m.pos[2].x, v.m.pos[2].y)
+						gl.TexCoord(v.t[3].x, v.t[3].y) gl.Vertex(v.m.pos[3].x, v.m.pos[3].y)
+						if v.m.pos[4] then
+							gl.TexCoord(v.t[4].x, v.t[4].y) gl.Vertex(v.m.pos[4].x, v.m.pos[4].y)
+						end
+					gl.End()
+				end
+			end
+		end
+
+		-- aiming aids
+		gl.EnableClientState("VERTEX_ARRAY")
+		for _, v in pairs(c_world_getTanks()) do
+			if v.exists then
+			if (v.weapon and v.weapon.aimAid) or (v.cd.aimAid) then
+					gl.PushAttrib("ENABLE_BIT")
+						local b
+						local vec = tankbobs.m_vec2()
+						local start, endP = tankbobs.m_vec2(v.p[1]), tankbobs.m_vec2()
+		
+						vec.t = v.r
+						vec.R = c_const_get("aimAid_startDistance")
+						start:add(vec)
+		
+						endP(start)
+						vec.R = c_const_get("aimAid_maxDistance")
+						endP:add(vec)
+		
+						b, vec = c_world_findClosestIntersection(start, endP)
+						if b then
+							endP = vec
+						end
+		
+						a[1][1] = start.x a[1][2] = start.y
+						a[2][1] = endP.x a[2][2] = endP.y
+						gl.Disable("TEXTURE_2D")
+						gl.Color(0.9, 0.1, 0.1, 1)
+						gl.TexEnv("TEXTURE_ENV_COLOR", 0.9, 0.1, 0.1, 1)
+						gl.VertexPointer(a)
+						gl.LineWidth(c_const_get("aimAid_width"))
+						gl.DrawArrays("LINES", 0, 2)
+					gl.PopAttrib()
+				end
+			end
+		end
+		gl.DisableClientState("VERTEX_ARRAY")
+
+		-- teleporters are drawn on top everything above
+		--gl.CallLists(play_teleporter_listsMultiple)
+
+		-- powerups are drawn next
+		for _, v in pairs(c_world_getPowerups()) do
 			gl.PushMatrix()
+				local c = c_world_getPowerupTypeByName(v.typeName).c
+				gl.Color(c.r, c.g, c.b, c.a)
+				gl.TexEnv("TEXTURE_ENV_COLOR", c.r, c.g, c.b, c.a)
 				gl.Translate(v.p[1].x, v.p[1].y, 0)
-				gl.Rotate(tankbobs.m_degrees(v.r) + c_const_get("healthbar_rotation"), 0, 0, 1)
-				if not (c_config_get("config.game.player" .. tostring(k) .. ".color", nil, true)) then
-					c_config_set("config.game.player" .. tostring(k) .. ".color.r", c_config_get("config.game.defaultTankRed"))
-					c_config_set("config.game.player" .. tostring(k) .. ".color.g", c_config_get("config.game.defaultTankBlue"))
-					c_config_set("config.game.player" .. tostring(k) .. ".color.b", c_config_get("config.game.defaultTankGreen"))
-					c_config_set("config.game.player" .. tostring(k) .. ".color.a", c_config_get("config.game.defaultTankAlpha"))
-				end
-
-				gl.Color(v.color.r, v.color.g, v.color.b, 1)
-				gl.TexEnv("TEXTURE_ENV_COLOR", v.color.r, v.color.g, v.color.b, 1)
-				local scale = v.health / c_const_get("tank_health")
-				if scale < 1 then
-					scale = 1
-				end
-				gl.Scale(scale, 1, 1)
-				gl.CallList(healthbarBorder_listBase)
-				gl.Scale(1 / scale, 1, 1)
-				if v.health >= c_const_get("tank_highHealth") then
-					gl.Color(0.1, 1, 0.1, 1)
-					gl.TexEnv("TEXTURE_ENV_COLOR", 0.1, 1, 0.1, 1)
-				elseif v.health > c_const_get("tank_lowHealth") then
-					gl.Color(1, 1, 0.1, 1)
-					gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 0.1, 1)
-				else
-					gl.Color(1, 0.1, 0.1, 1)
-					gl.TexEnv("TEXTURE_ENV_COLOR", 1, 0.1, 0.1, 1)
-				end
-				gl.Scale(v.health / c_const_get("tank_health"), 1, 1)
-				gl.CallList(healthbar_listBase)
+				gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
+				gl.CallList(powerup_listBase)
 			gl.PopMatrix()
 		end
-	end
+
+		-- projectiles
+		for _, v in pairs(c_weapon_getProjectiles()) do
+			if v.weapon.trail == 0 then  -- only draw the trail
+				gl.PushMatrix()
+					gl.Translate(v.p[1].x, v.p[1].y, 0)
+					gl.Rotate(tankbobs.m_degrees(v.r), 0, 0, 1)
+					gl.CallList(v.weapon.m.p.projectileList)
+				gl.PopMatrix()
+			end
+		end
+
+		-- trails
+		for k, v in pairs(trails) do
+			-- {time left, maximum intensity, list}
+			v[1] = v[1] - d
+			if v[1] <= 0 then
+				gl.DeleteLists(v[3], 1)
+
+				table.remove(trails, k)
+
+				return
+			end
+
+			gl.Color(1, 1, 1, v[1] / v[2])
+			gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 1, v[2] / v[3])
+			gl.CallList(v[3])
+		end
+
+		-- healthbars
+		for k, v in pairs(c_world_getTanks()) do
+			if v.exists then
+				gl.PushMatrix()
+					gl.Translate(v.p[1].x, v.p[1].y, 0)
+					gl.Rotate(tankbobs.m_degrees(v.r) + c_const_get("healthbar_rotation"), 0, 0, 1)
+					if not (c_config_get("config.game.player" .. tostring(k) .. ".color", nil, true)) then
+						c_config_set("config.game.player" .. tostring(k) .. ".color.r", c_config_get("config.game.defaultTankRed"))
+						c_config_set("config.game.player" .. tostring(k) .. ".color.g", c_config_get("config.game.defaultTankBlue"))
+						c_config_set("config.game.player" .. tostring(k) .. ".color.b", c_config_get("config.game.defaultTankGreen"))
+						c_config_set("config.game.player" .. tostring(k) .. ".color.a", c_config_get("config.game.defaultTankAlpha"))
+					end
+
+					gl.Color(v.color.r, v.color.g, v.color.b, 1)
+					gl.TexEnv("TEXTURE_ENV_COLOR", v.color.r, v.color.g, v.color.b, 1)
+					local scale = v.health / c_const_get("tank_health")
+					if scale < 1 then
+						scale = 1
+					end
+					gl.Scale(scale, 1, 1)
+					gl.CallList(healthbarBorder_listBase)
+					gl.Scale(1 / scale, 1, 1)
+					if v.health >= c_const_get("tank_highHealth") then
+						gl.Color(0.1, 1, 0.1, 1)
+						gl.TexEnv("TEXTURE_ENV_COLOR", 0.1, 1, 0.1, 1)
+					elseif v.health > c_const_get("tank_lowHealth") then
+						gl.Color(1, 1, 0.1, 1)
+						gl.TexEnv("TEXTURE_ENV_COLOR", 1, 1, 0.1, 1)
+					else
+						gl.Color(1, 0.1, 0.1, 1)
+						gl.TexEnv("TEXTURE_ENV_COLOR", 1, 0.1, 0.1, 1)
+					end
+					gl.Scale(v.health / c_const_get("tank_health"), 1, 1)
+					gl.CallList(healthbar_listBase)
+				gl.PopMatrix()
+			end
+		end
+	gl.PopMatrix()
 
 	-- scores, FPS, rest of HUD, etc.
 	gui_paint(d)
