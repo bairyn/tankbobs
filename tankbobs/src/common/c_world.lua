4dc37da33d2e71d7645b96b6b46f6a6285500c04
bob <KrabbyKrap@gmail.com>
2009-10-24 18:57:56 -0600

    Preserv velocity when a corpse is created

--- tankbobs/src/common/c_world.lua
+++ tankbobs/src/common/c_world.lua
@@ -683,12 +683,15 @@ function c_world_addCorpse(tank)
 	local FIXMETODOtmpLastAttackers = tank.lastAttackers  -- HACK: work around segfault
 	tank.lastAttackers = {}  -- FIXME TODO: t_clone() segfaults when this isn't set?
 
+	local vel = t_w_getLinearVelocity(tank.body)
+
 	t_t_clone(true, tank, corpse)
 
 	tank.lastAttackers = FIXMETODOtmpLastAttackers
 
 	corpse.explodeTime = t_t_getTicks() + c_world_timeMultiplier(c_const_get("world_corpseTime"))
 	corpse.body = tankbobs.w_addBody(corpse.p, corpse.r, c_const_get("corpse_canSleep"), c_const_get("corpse_isBullet"), c_const_get("corpse_linearDamping"), c_const_get("corpse_angularDamping"), corpse.h, c_const_get("corpse_density"), c_const_get("corpse_friction"), c_const_get("corpse_restitution"), not c_const_get("corpse_static"), c_const_get("corpse_contentsMask"), c_const_get("corpse_clipmask"), c_const_get("corpse_isSensor"), index)
+	t_w_setLinearVelocity(corpse.body, vel)
 
 	corpse.exists = true
 
