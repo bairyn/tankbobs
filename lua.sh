1d16cddf81d9adc1db176d0ef55e9959ebada59e
bob <KrabbyKrap@gmail.com>
2009-11-22 22:58:31 +0000

    Change to the directory in which the line counter scripts lie before counting

--- lua.sh
+++ lua.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd `dirname $0`
 find .. | grep -i "\.lua\$" | grep -vi "Box2D" | grep -vi "git" | grep -vi "CMakeC" | grep -vi "qrc" | grep -vi "moc_" | grep -vi "ui_" | grep -vi "build" | grep -vi "summary.lua" | xargs wc -l
