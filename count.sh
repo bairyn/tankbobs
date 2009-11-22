361e59fa7d884349530d014d0e2af7a8304961a3
bob <KrabbyKrap@gmail.com>
2009-11-22 23:22:53 +0000

    exclude physfs from line count

--- count.sh
+++ count.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-find .. | grep -i "\.\(lua\|c\|cpp\|h\)\$" | grep -vi "Box2D" | grep -vi "git" | grep -vi "CMakeC" | grep -vi "qrc" | grep -vi "moc_" | grep -vi "ui_" | grep -vi "build" | grep -vi "summary.lua" | grep -vi "LuaJIT" | xargs wc -l
+find .. | grep -i "\.\(lua\|c\|cpp\|h\)\$" | grep -vi "Box2D" | grep -vi "git" | grep -vi "CMakeC" | grep -vi "qrc" | grep -vi "moc_" | grep -vi "ui_" | grep -vi "build" | grep -vi "physfs" | grep -vi "summary.lua" | grep -vi "LuaJIT" | xargs wc -l
