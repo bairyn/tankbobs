ab038928dbd89c22a7e22d00f1c1418d00685fb3
bob <KrabbyKrap@gmail.com>
2010-01-13 21:43:39 +0000

    Set last map and set on startup

--- tankbobs/mod-client/reset-last-map.lua
+++ tankbobs/mod-client/reset-last-map.lua
@@ -2,3 +2,5 @@
 -- set last map and last set to default
 c_mods_appendFunction("c_tcm_select_set", function () c_config_set("game.lastSet", "small") end)
 c_mods_appendFunction("c_tcm_select_map", function () c_config_set("game.lastMap", "small_1") end)
+c_config_set("game.lastSet", "small")
+c_config_set("game.lastMap", "small_1")
