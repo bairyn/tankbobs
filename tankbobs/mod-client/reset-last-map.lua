069d9d53be344c3ef6f5092c7ad41d8ed3768b00
bob <KrabbyKrap@gmail.com>
2010-01-13 21:24:04 +0000

    Fix preview workaround

--- tankbobs/mod-client/reset-last-map.lua
+++ tankbobs/mod-client/reset-last-map.lua
@@ -1,4 +1,4 @@
 -- This is a work around while AI in larger levels is broken
 -- set last map and last set to default
-c_config_set("game.lastMap", "small_1")
-c_config_set("game.lastSet", "small")
+c_mods_appendFunction("c_tcm_select_set", function () c_config_set("game.lastSet", "small") end)
+c_mods_appendFunction("c_tcm_select_map", function () c_config_set("game.lastMap", "small_1") end)
