248b16d4e502c649ed2453ecfa66cf166d0975a8
Byron Johnson <KrabbyKrap@gmail.com>
2010-03-04 03:01:23 +0000

    tstr includes and links with strsafe on windows

--- tankbobs/src/lib/crossdll.c
+++ tankbobs/src/lib/crossdll.c
@@ -32,6 +32,7 @@ along with Tankbobs.  If not, see <http://www.gnu.org/licenses/>.
 #include "crossdll.h"
 #if defined(__WINDOWS__) || defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__)
 #include <windows.h>
+#include <strsafe.h>
 static const char *extension = ".dll";
 #else
 #include <dlfcn.h>
