d6b5066ee1830404908b8784bb16b788b469e0c0
Byron Johnson <KrabbyKrap@gmail.com>
2010-06-10 01:13:14 +0000

    Fix previous commit

--- tankbobs/src/common/c_world.lua
+++ tankbobs/src/common/c_world.lua
@@ -1308,7 +1308,9 @@ function c_world_tank_checkSpawn(d, tank)
 				end
 			end
 
-			tank.lastSpawnPoint = (index - 1) % #c_tcm_current_map.powerupSpawnPoints + 1
+			tank.lastSpawnPoint = (index - 1) % #c_tcm_current_map.playerSpawnPoints + 1
+
+			tank.lastSpawnPoint = tank.lastSpawnPoint - 1
 		end
 	end
 
