04e6ad9fd4d784a2fc9cf4948a642c86b9fdcfe1
bairyn <byron@byronjohnson.net>
2014-05-09 06:14:48 -0700

    Further maintenance: rewrite build script.

--- tankbobs/build.sh
+++ tankbobs/build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-SERVER_SRCS="src/common/common.lua src/common/c_class.lua src/common/c_config.lua src/common/c_const.lua src/common/c_data.lua src/common/c_math.lua src/common/c_mods.lua src/common/c_module.lua src/common/c_state.lua src/common/c_files.lua src/common/c_tcm.lua src/common/c_weapon.lua src/common/c_ai.lua src/common/c_world.lua src/common/c_protocol.lua src/common/lom.lua src/server/init.lua src/server/main.lua src/server/commands.lua src/server/client.lua src/server/st_main.lua"
-CLIENT_SRCS="src/common/common.lua src/common/c_class.lua src/common/c_config.lua src/common/c_const.lua src/common/c_data.lua src/common/c_math.lua src/common/c_mods.lua src/common/c_module.lua src/common/c_state.lua src/common/c_files.lua src/common/c_tcm.lua src/client/game.lua src/common/c_weapon.lua src/common/c_ai.lua src/common/c_world.lua src/common/c_protocol.lua src/common/lom.lua src/client/gui.lua src/client/init.lua src/client/main.lua src/client/renderer.lua src/client/st_exit.lua src/client/st_help.lua src/client/st_level.lua src/client/st_background.lua src/client/st_internet.lua src/client/st_online.lua src/client/st_options.lua src/client/st_play.lua src/client/st_selected.lua src/client/st_set.lua src/client/st_title.lua"
-OPT_SRCS="src/lib/LuaJIT/jit/opt.lua src/lib/LuaJIT/jit/opt_inline.lua"
-DATA="BUILDFILES CHANGELOG COPYING data mod-client mod-server NOTICE modules modules64 modules-win modules64-win"
-COPYDATA="mod-client mod-server"
-VERSION="0.1.0"
+set -e
 
 cd `dirname $0`
 
+SERVER_SRCS="src/common/common.lua src/common/c_class.lua src/common/c_config.lua src/common/c_const.lua src/common/c_data.lua src/common/c_math.lua src/common/c_mods.lua src/common/c_module.lua src/common/c_state.lua src/common/c_files.lua src/common/c_tcm.lua src/common/c_weapon.lua src/common/c_ai.lua src/common/c_world.lua src/common/c_protocol.lua src/common/lom.lua src/server/init.lua src/server/main.lua src/server/commands.lua src/server/client.lua src/server/st_main.lua"
+CLIENT_SRCS="src/common/common.lua src/common/c_class.lua src/common/c_config.lua src/common/c_const.lua src/common/c_data.lua src/common/c_math.lua src/common/c_mods.lua src/common/c_module.lua src/common/c_state.lua src/common/c_files.lua src/common/c_tcm.lua src/client/game.lua src/common/c_weapon.lua src/common/c_ai.lua src/common/c_world.lua src/common/c_protocol.lua src/common/lom.lua src/client/gui.lua src/client/init.lua src/client/main.lua src/client/renderer.lua src/client/st_exit.lua src/client/st_help.lua src/client/st_level.lua src/client/st_background.lua src/client/st_internet.lua src/client/st_online.lua src/client/st_options.lua src/client/st_play.lua src/client/st_selected.lua src/client/st_set.lua src/client/st_title.lua"
+OPT_SRCS="src/lib/LuaJIT/jit/opt.lua src/lib/LuaJIT/jit/opt_inline.lua"
+COPYDATA="mod-client mod-server"
+VERSION=$(cat VERSION)
+ZIPFLAGS="-9r"
+
+# Files to copy to build directory.
+DATAFILES="CHANGELOG COPYING NOTICE mod-client mod-server modules modules64 modules-win modules64-win data"
+# Files to archive in optional binary distribution.
+DISTFILES="CHANGELOG COPYING NOTICE mod-client mod-server modules modules64 modules-win modules64-win client server libmtankbobs.so libtstr.so jit data-tankbobs-v*-r*.tpk tankbobs* trmc*"
+
 REVISION=$(git rev-list master | wc -l)
-PAKNAME="data-tankbobs-v${VERSION}-R${REVISION}.tpk"
-BUILDNAME="tankbobs-build-v${VERSION}-R${REVISION}.tar.gz"
+PAKNAME="data-tankbobs-v${VERSION}-r${REVISION}.tpk"
+BUILDNAME="tankbobs-build-v${VERSION}-r${REVISION}.tar.gz"
 CMAKEFLAGS="-D PEDANTIC=TRUE $OTHER_CMAKE_FLAGS"
 
 LUAC=luac5.1
@@ -23,246 +30,132 @@ fi
 cd ./build
 
 debug=0
-skipc=0
 nojit=0
+cdist=0
+phelp=0
 
-if [ "$1" == "-d" ]; then
-	debug=1
-	shift
+while getopts dnth\? opt; do
+	case $opt in
+	d)
+		debug=1
+		;;
+	n)
+		nojit=1
+		;;
+	t)
+		cdist=1
+		;;
+	h)
+		phelp=1
+		;;
+	\?)
+		phelp=1
+		;;
+	esac
+done
+
+if [ "$phelp" == 1 ]; then
+  cat <<EOM
+Build Tankbobs:
+
+1) Compiles lua.
+2) Compiles third party libraries.
+3) Wraps cmake to build Tankbobs.
+4) Copies data files to build directory.
+5) Compiles maps.
+6) Creates data archive containing compiled maps.
+7) Optionally creates binary tarball containing the build.
+
+Usage: $0 [-d] [-n] [-t] [MAKE ARGS]
+
+Example: ./build.sh -d -n -t VERBOSE=1
+
+-d: Enable debugging
+-n: Disable Lua JIT
+-t: Create binary tarball containing the build
+EOM
+	exit 1
 fi
 
-if [ "$1" == "-n" ]; then
-	skipc=1
-	shift
-fi
+cd ./../
 
-if [ "$1" == "-d" ]; then
-	debug=1
-	shift
-fi
-
-if [ "$1" == "-o" ]; then
-	nojit=1
-	shift
-fi
-
-if [ "$1" == "make" ]; then
-	arg_1=$1
-
-	cd ./../
-
-	# build lua manually
-	if [ $debug == 0 ]; then
-		echo "$LUAC -s -o ./build/server ${SERVER_SRCS}"
-		if ! $LUAC -s -o ./build/server ${SERVER_SRCS}; then
-			exit 1
-		fi
-		echo "$LUAC -s -o ./build/server ${CLIENT_SRCS}"
-		if ! $LUAC -s -o ./build/client ${CLIENT_SRCS}; then
-			exit 1
-		fi
-	else
-		echo "$LUAC -o ./build/server ${SERVER_SRCS}"
-		if ! $LUAC -o ./build/server ${SERVER_SRCS}; then
-			exit 1
-		fi
-		echo "$LUAC -o ./build/server ${CLIENT_SRCS}"
-		if ! $LUAC -o ./build/client ${CLIENT_SRCS}; then
-			exit 1
-		fi
-	fi
-
-	# build Box2D and luaJIT manually
-	if [ $debug == 0 ]; then
-		make -C ./src/lib/Box2D/Source CXXFLAGS="-O2 -fPIC"
-		make -C ./src/lib/LuaJIT linux
-		if ! [ -d "./build/jit" ]; then
-			mkdir ./build/jit
-		fi
-		cp ./src/lib/LuaJIT/jit/opt.lua ./build/jit/
-		cp ./src/lib/LuaJIT/jit/opt_inline.lua ./build/jit/
-	else
-		make -C ./src/lib/Box2D/Source CXXFLAGS="-g -fPIC"
-		make -C ./src/lib/LuaJIT linux CFLAGS="-DLUAJIT_ASSERT -DLUA_USE_APICHECK -DUSE_VALGRIND -g -fomit-frame-pointer -Wall -DLUA_USE_LINUX -I../dynasm"
-		if ! [ -d "./build/jit" ]; then
-			mkdir ./build/jit
-		fi
-		cp ./src/lib/LuaJIT/jit/opt.lua ./build/jit/
-		cp ./src/lib/LuaJIT/jit/opt_inline.lua ./build/jit/
-	fi
-
-	# build physfs (PhysicsFS)
-	cd ./src/lib/physfs-2.0.0/
-	#if ! cmake .; then
-	if ! cmake -Wno-dev -DPHYSFS_BUILD_WX_TEST=FALSE .; then  # Remove annoying warning
-		exit 1
-	fi
-	if ! make -C .; then
-		exit 1
-	fi
-	cd ./../../../
-
-	# build tankbobs
-	cd ./build/
-	if [ $skipc == 0 ]; then
-		if [ $debug == 0 ]; then
-			if [ $nojit == 0 ]; then
-				if ! cmake -D CMAKE_BUILD_TYPE=Release -D NOJIT=False ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			else
-				if ! cmake -D CMAKE_BUILD_TYPE=Release -D NOJIT=True ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			fi
-		else
-			if [ $nojit == 0 ]; then
-				if ! cmake -D CMAKE_BUILD_TYPE=Debug -D NOJIT=False ${CMAKEFLAGS} -D TDEBUG=TRUE ./../; then
-					exit 1
-				fi
-			else
-				if ! cmake -D CMAKE_BUILD_TYPE=Debug -D NOJIT=True ${CMAKEFLAGS} -D TDEBUG=TRUE ./../; then
-					exit 1
-				fi
-			fi
-		fi
-	fi
-
-	cd ./../
-
-	# remove old data
-	if [ -d "./build/data" ]; then
-		if ! rm -r ./build/data; then
-			exit 1
-		fi
-	fi
-
-	# copy data
-	if ! cp -R --preserve=all -t ./build/ ${DATA}; then
-		exit 1
-	fi
-
-	cd ./build/
-
-	# make it
-	if ! eval $*; then
-		exit 1
-	fi
-
-	# compile maps
-	if [ -f "./trmc" ] && [ -d "./data/tcm/" ]; then
-		echo -ne "./trmc -f" `find ./data/tcm/ -name "*.trm"`"\n"
-		if ! find ./data/tcm/ -name "*.trm" -exec ./trmc -f {} +; then
-			exit 1
-		fi
-
-		echo -ne "rm" `find ./data/tcm/ -name "*.trm"`"\n"
-		# remove source maps
-		if ! find ./data/tcm/ -name "*.trm" -exec rm {} +; then
-			exit 1
-		fi
-	else
-		echo -ne "Warning: level compiler not built\n"
-	fi
-
-	# make ~/.tankbobs if it doesn't exist
-	if ! [ -d "${HOME}/.tankbobs" ]; then
-		if [ -e "${HOME}/.tankbobs" ]; then
-			echo -ne "file \"${HOME}/.tankbobs \" is not a directory\n"
-			exit 1
-		else
-			if ! mkdir "${HOME}/.tankbobs"; then
-				exit 1
-			fi
-
-			echo -ne "Made directory \"${HOME}/.tankbobs\"\n"
-		fi
-	fi
-
-	# remove old packages
-	rm ./data-tankbobs-*.tpk
-	rm ./tankbobs-build-*.tar.gz
-
-	echo -ne "Packing data"
-
-	# pak data
-	cd ./data/
-	if ! zip -9r "./../${PAKNAME}" *; then
-		exit 1
-	fi
-	cd ./../
-	#if ! cp "./${PAKNAME}" ${HOME}/.tankbobs/; then
-		#exit 1
-	#fi
-	#if ! cp -R ${COPYDATA} ${HOME}/.tankbobs/; then
-		#exit 1
-	#fi
-
-	# copy files listed in BUILDFILES to ./dist/
-
-	echo -ne "Creating \"${BUILDNAME}\"\n"
-
-	if [ -d ./dist_pk_tmp ]; then
-		echo -ne "Recursively removing \"./dist_pk_tmp\"\n"
-		if ! rm -r ./dist_pk_tmp; then
-			exit 1
-		fi
-	elif [ -e ./dist_pk_tmp ]; then
-		echo -ne "\"./dist_pk_tmp\" is not a directory\n"
-		exit 1
-	fi
-
-	if ! mkdir ./dist_pk_tmp; then
-		exit 1
-	fi
-
-	for FILE in `cat ./BUILDFILES`; do
-		if ! cp -L -R ${FILE} ./dist_pk_tmp/; then
-			exit 1
-		fi
-	done
-
-	cd ./dist_pk_tmp/
-	if ! tar -zcf ./../${BUILDNAME} ./*; then
-		exit 1
-	fi
-	cd ./../
-
-	if [ -d ./dist_pk_tmp ]; then
-		if ! rm -r ./dist_pk_tmp; then
-			exit 1
-		fi
-	fi
-
-	echo -ne "Build ended successfully\n"
-elif [ "$1" == "-h" ]; then
-	echo -ne "Usage: $0 (-d to debug) (-n to skip cmake) (-o to disable jit) (make (flags to pass to make))\nUse make VERBOSE=1 for verbose output\nOptions need to be in order\n"
+PHYSFS_CMAKE_FLAGS="-Wno-dev -DPHYSFS_BUILD_WX_TEST=FALSE $PHYSFS_CMAKE_FLAGS"
+if [ $debug == 0 ]; then
+	LUAFLAGS="$LUAFLAGS"
+	BOX2D_CXXFLAGS="-O2 -fPIC $BOX2D_CXXFLAGS"
+	TANKBOBS_CMAKE_FLAGS=" $PHYSFS_CMAKE_FLAGS"
+	CMAKEFLAGS="-D CMAKE_BUILD_TYPE=Release $CMAKEFLAGS"
 else
-	# just cmake
-
-	echo -ne "Preparing build only (see -h for usage)\n"
-
-	if [ $skipc == 0 ]; then
-		if [ $debug == 0 ]; then
-			if [ $nojit == 0 ]; then
-				if ! cmake -D CMAKE_BUILD_TYPE=Release -D NOJIT=False ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			else
-				if ! cmake -D CMAKE_BUILD_TYPE=Release -D NOJIT=True ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			fi
-		else
-			if [ $nojit == 0 ]; then
-				if ! cmake -D CMAKE_BUILD_TYPE=Debug -D NOJIT=False ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			else
-				if ! cmake -D CMAKE_BUILD_TYPE=Release -D NOJIT=True ${CMAKEFLAGS} ./../; then
-					exit 1
-				fi
-			fi
-		fi
-	fi
+	LUAFLAGS="-s $LUAFLAGS"
+	BOX2D_CXXFLAGS="-g -fPIC $BOX2D_CXXFLAGS"
+	TANKBOBS_CMAKE_FLAGS=" $PHYSFS_CMAKE_FLAGS"
+	CMAKEFLAGS="-D CMAKE_BUILD_TYPE=Debug $CMAKEFLAGS"
 fi
+
+if [ $nojit == 0 ]; then
+	CMAKEFLAGS="-D NOJIT=False $CMAKEFLAGS"
+else
+	CMAKEFLAGS="-D NOJIT=True $CMAKEFLAGS"
+fi
+
+set -x
+mkdir -p build
+
+# Build lua.
+
+$LUAC $LUAFLAGS -o ./build/server ${SERVER_SRCS}
+$LUAC $LUAFLAGS -o ./build/client ${CLIENT_SRCS}
+
+
+# Build dependencies.
+
+## Box2D
+make -C ./src/lib/Box2D/Source CXXFLAGS="$BOX2D_CXXFLAGS"
+
+## LuaJIT
+if [ $nojit == 0 ]; then
+	make -C ./src/lib/LuaJIT linux
+fi
+
+## PhysFS
+cd ./src/lib/physfs-2.0.0/
+cmake $PHYSFS_CMAKE_FLAGS .
+make -C .
+cd $OLDPWD
+
+
+# Call cmake for Tankbobs.
+
+cd ./build/
+cmake $CMAKEFLAGS ./../
+make -C .
+cd $OLDPWD
+
+
+# Copy data files to build directory.
+rm -fr ./build/data
+cp -a -t ./build/ $DATAFILES
+
+
+# Compile maps.
+find ./build/data/tcm/ -name '*.trm' -exec ./build/trmc -f {} +
+find ./build/data/tcm/ -name '*.trm' -exec rm              {} +
+
+
+# Pack data archive.
+cd ./build/data/
+find .. -maxdepth 1 -name 'data-tankbobs-*.tpk' -exec rm {} +
+zip $ZIPFLAGS "./../${PAKNAME}" *
+cd $OLDPWD
+
+
+# Optionally create binary tarball.
+if [ $cdist == 1 ]; then
+	cd ./build
+	find . -maxdepth 1 -name 'tankbobs-build-*.tar.gz' -exec rm {} +
+	tar -c -hz -f $BUILDNAME $(eval echo $DISTFILES)
+	cd $OLDPWD
+fi
+
+
+exit
